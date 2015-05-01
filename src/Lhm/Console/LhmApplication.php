10e9b688ec7ef851106be0c9abfa31aae47c078a
Martin Samson <martin@canvaspop.com>
2015-05-01 10:35:16 -0400

    Adding console command.

---
+++ src/Lhm/Console/LhmApplication.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace Lhm\Console;
+
+use Symfony\Component\Console\Application;
+
+
+class LhmApplication extends Application
+{
+    /**
+     * Class Constructor.
+     *
+     * Initialize the Lhm console application.
+     *
+     * @param string $version The Application Version
+     */
+    public function __construct($version = '0.3.0')
+    {
+        parent::__construct('Large Hadron Migrator', $version);
+
+        $this->addCommands([
+            new Command\Cleanup()
+        ]);
+    }
+}
